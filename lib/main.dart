ad848228fb8ed6fbfd9a8be7009bb211bd6c4bed
Ariel Ferrandin Morgan <arielmmgn@gmail.com>
2025-09-30 15:34:52 -0300

    feat: Implement locale-based date formatting and save locale preference

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,30 @@
+import 'dart:ui';
+
 import 'package:eco_simple_tasks/app.dart';
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+
+  Locale locale = PlatformDispatcher.instance.locale;
+  String appLocale =
+    locale.languageCode == 'pt' && locale.countryCode == 'BR'
+      ? 'pt_BR' 
+      : 'en_US';
+
+  // initialize date formatting for the selected locales
+  await initializeDateFormatting(appLocale, null);
+
+  // writes to storage
+  await saveLocale(appLocale);
+
   runApp(const App());
 }
 
+Future<void> saveLocale(String locale) async {
+  final box = GetStorage();
+  await box.write('app_locale', locale);
+}

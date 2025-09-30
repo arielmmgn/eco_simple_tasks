eaafdb69ae6c4d20a771d459dd1189fdd0aaf04f
Ariel Ferrandin Morgan <arielmmgn@gmail.com>
2025-09-30 14:19:40 -0300

    refactor: Update primary color and remove unused color definitions

--- lib/core/eco_colors.dart
+++ lib/core/eco_colors.dart
@@ -2,20 +2,11 @@ import 'package:flutter/material.dart';
 
 class EcoColors {
   static Color primary(double opacity) => 
-    Color.fromRGBO(159, 102, 233, opacity);
+    Color.fromRGBO(72, 43, 231, opacity);
 
   static Color white(double opacity) =>
     Color.fromRGBO(255, 255, 255, opacity);
 
-  static Color card(double opacity) =>
-    Color.fromRGBO(186, 241, 211, opacity);
-
-  static Color unselect(double opacity) =>
-    Color.fromRGBO(80, 71, 87, opacity);
-
   static Color black(double opacity) =>
     Color.fromRGBO(0, 0, 0, opacity);
-
-  static Color dark(double opacity) =>
-    Color.fromRGBO(24, 18, 29, opacity);
 }
